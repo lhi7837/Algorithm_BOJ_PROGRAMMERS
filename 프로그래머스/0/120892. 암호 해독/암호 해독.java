9e1fd762f6d52802830f006d6fe9e9df03ab9ff2
Haeil Lim <lhi7837@gmail.com>
2024-03-24 13:17:27 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = code-1; i < cipher.length(); i += code) {
+            sb.append(String.valueOf(cipher.charAt(i)));
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
