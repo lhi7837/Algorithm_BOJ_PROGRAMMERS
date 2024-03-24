6117049ee6e2c995c5fedf5c2ab4aa4a50d8ebc8
Haeil Lim <lhi7837@gmail.com>
2024-03-24 16:30:06 +0900

    [level 0] Title: 대문자와 소문자, Time: 6.87 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i = 0; i < my_string.length(); i++) {
+            if(my_string.charAt(i) >= 'a' && my_string.charAt(i) <= 'z') {
+                answer += my_string.substring(i, i+1).toUpperCase();
+            }
+            else {
+                answer += my_string.substring(i, i+1).toLowerCase();
+            }
+        }
+        return answer;
+    }
+}
