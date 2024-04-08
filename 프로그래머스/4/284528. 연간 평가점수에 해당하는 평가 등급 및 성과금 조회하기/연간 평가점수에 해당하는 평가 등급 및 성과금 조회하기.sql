7d4394b75e4302557a362f530564a38888f58cbc
Haeil Lim <lhi7837@gmail.com>
2024-04-08 14:16:08 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,23 @@
+SELECT 
+    E.EMP_NO, 
+    E.EMP_NAME, 
+    G.GRADE,
+    (CASE GRADE WHEN "S" THEN SAL * 0.2
+        WHEN "A" THEN SAL * 0.15
+        WHEN "B" THEN SAL * 0.1
+        ELSE 0 END) AS BONUS
+FROM 
+    HR_EMPLOYEES E
+LEFT JOIN
+    (SELECT 
+        EMP_NO, 
+        AVG(SCORE) AS SCORE, 
+        (CASE WHEN AVG(SCORE) >= 96 THEN "S"
+        WHEN AVG(SCORE) >= 90 THEN "A" 
+        WHEN AVG(SCORE) >= 80 THEN "B" 
+        ELSE "C" END) AS GRADE 
+    FROM HR_GRADE
+    GROUP BY EMP_NO) G
+    ON E.EMP_NO = G.EMP_NO
+GROUP BY EMP_NO, EMP_NAME
+ORDER BY EMP_NO ASC;
