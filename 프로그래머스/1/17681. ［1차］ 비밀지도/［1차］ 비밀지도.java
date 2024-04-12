dcfb44408f216751d877ae397a26bf7adfa43008
Haeil Lim <lhi7837@gmail.com>
2024-04-13 00:26:21 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[arr1.length];
+        String[] arr1_str = new String[arr1.length];
+        String[] arr2_str = new String[arr2.length];
+        int idx = 0;
+        for(int i : arr1) {
+            arr1_str[idx++] = Integer.toBinaryString(i); 
+        }
+        idx = 0;
+        for(int i : arr2) {
+            arr2_str[idx++] = Integer.toBinaryString(i); 
+        }
+        for(int i = 0; i < answer.length; i++) {
+            StringBuffer sb = new StringBuffer();
+            int temp = arr1[i] | arr2[i];
+            String binary = Integer.toBinaryString(temp);
+            for(int j = 0; j < binary.length(); j++) {
+                if(binary.charAt(j) == '1') sb.append("#");
+                else sb.append(" ");
+            }
+            answer[i] = sb.toString();
+            if(answer[i].length() < answer.length) {
+                StringBuffer sb_blank = new StringBuffer();
+                for(int j = 0; j < answer.length - answer[i].length(); j++) {
+                    sb_blank.append(" ");
+                }
+                answer[i] = sb_blank.toString() + sb.toString();
+            }
+        }
+        return answer;
+    }
+}
