23c664431eb897e269b58e173f7d6e4f0315bf86
Haeil Lim <lhi7837@gmail.com>
2024-04-13 00:27:50 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,8 +1,8 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[arr1.length];
-        String[] arr1_str = new String[arr1.length];
-        String[] arr2_str = new String[arr2.length];
+        String[] answer = new String[n];
+        String[] arr1_str = new String[n];
+        String[] arr2_str = new String[n];
         int idx = 0;
         for(int i : arr1) {
             arr1_str[idx++] = Integer.toBinaryString(i); 
@@ -11,7 +11,7 @@ class Solution {
         for(int i : arr2) {
             arr2_str[idx++] = Integer.toBinaryString(i); 
         }
-        for(int i = 0; i < answer.length; i++) {
+        for(int i = 0; i < n; i++) {
             StringBuffer sb = new StringBuffer();
             int temp = arr1[i] | arr2[i];
             String binary = Integer.toBinaryString(temp);
@@ -20,9 +20,9 @@ class Solution {
                 else sb.append(" ");
             }
             answer[i] = sb.toString();
-            if(answer[i].length() < answer.length) {
+            if(answer[i].length() < n) {
                 StringBuffer sb_blank = new StringBuffer();
-                for(int j = 0; j < answer.length - answer[i].length(); j++) {
+                for(int j = 0; j < n - answer[i].length(); j++) {
                     sb_blank.append(" ");
                 }
                 answer[i] = sb_blank.toString() + sb.toString();
