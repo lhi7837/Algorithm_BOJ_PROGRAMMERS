cbf7ffea384d16c1b8b443b032630632334f89be
Haeil Lim <lhi7837@gmail.com>
2024-04-17 17:40:15 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.96 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,6 +1,3 @@
-class Solution {
-    public int[] solution(int n, long left, long right) {
-        int[] answer = new int[(int)(right-left) + 1];
         //인덱스 i 또는 j에 들어간 값 중 가장 큰 값 ==> 해당 인덱스가 갖고 있는 값
         //int[3,3] ==> 3, int[3,2] ==> 3
         //n=3 으로 size를 나눌 때 --> [0]부터[8]까지
@@ -8,11 +5,13 @@ class Solution {
             // 123 ==> 몫 0, 나머지 0 ,1, 2
             // 223 ==> 몫 1, 나머지 0, 1, 2
             // 333 ==> 몫 2, 나머지 0, 1, 2
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right-left) + 1];
         int x = 0;
         int y = 0;
         int idx = 0;
         while(idx < answer.length) {
-        //이부분을 해결하면됨
             for(long j = left; j <= right; j++) {
                 x = (int)(j % n);
                 y = (int)(j / n);
