371f77c38ea717dc82a862ec40afa85177681748
Haeil Lim <lhi7837@gmail.com>
2024-04-29 14:12:55 +0900

    [Silver III] Title: N과 M (4), Time: 148 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(1, 0);
+        System.out.print(sb);
+    }
+
+    public static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+        		arr[depth] = i;
+        		dfs(i, depth + 1);
+        	}
+        }
+}
