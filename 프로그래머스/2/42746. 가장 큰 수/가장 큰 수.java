e48aa7fa4ea44f2224b4a366c70e135e27b107d4
Haeil Lim <lhi7837@gmail.com>
2024-07-30 15:20:45 +0900

    [level 2] Title: 가장 큰 수, Time: 318.64 ms, Memory: 133 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = 9; j >= 0; j--) {
+                if(numbers[i].charAt(0) == '9')
+                
+            }
+        }
+        return answer;
+    }
+}
