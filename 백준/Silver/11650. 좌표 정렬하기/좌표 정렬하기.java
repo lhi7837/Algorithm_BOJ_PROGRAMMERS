9ac13a7cde25769068aa2ec9ce2137d8395f0b15
hhhhhhhhhi <lhi7837@gmail.com>
2024-10-07 15:50:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 676 ms, Memory: 56776 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -9,29 +9,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][2];
-        Map<Integer, List<Integer>> map = new TreeMap<>();
         for(int i = 0; i < N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int key = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
-            if(map.containsKey(key)){
-                map.get(key).add(value);
-            }
-            else {
-                List<Integer> list = new ArrayList<>();
-                list.add(value);
-                map.put(key, list);
-            }
-//            System.out.println("s : "+ Arrays.toString(arr[i]));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        for(int key : map.keySet()){
-            List<Integer> list = map.get(key);
-            Collections.sort(list);
-            for(int i : list) {
-                sb.append(key).append(" ").append(i).append("\n");
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
             }
-        }
-
+        });
+        // 출력
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append("\n");
+		}
         System.out.println(sb.toString());
     }
 }
