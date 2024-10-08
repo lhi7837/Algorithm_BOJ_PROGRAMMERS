2957b6516de75de9ac9142fb281c72650e4cb82d
hhhhhhhhhi <lhi7837@gmail.com>
2024-10-08 11:10:49 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 6.05 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,19 +1,18 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] numbers) {
-        Set<Integer> answer_set = new HashSet<>();
-        for(int i = 0; i < numbers.length; i++) {
+        Arrays.sort(numbers);
+        Set<Integer> set = new TreeSet<>();
+        for(int i = 0; i < numbers.length - 1; i++) {
             for(int j = i + 1; j < numbers.length; j++) {
-                answer_set.add(numbers[i] + numbers[j]);
+                set.add(numbers[i] + numbers[j]);
             }
         }
-        int[] answer = new int[answer_set.size()];
+        int[] answer = new int[set.size()];//개수는 어떻게 구할 것인가? 리스트는 써도 되는건가?
         int idx = 0;
-        for(int i : answer_set) {
+        for(int i : set) {
             answer[idx++] = i;
         }
-        Arrays.sort(answer);
         return answer;
     }
 }
