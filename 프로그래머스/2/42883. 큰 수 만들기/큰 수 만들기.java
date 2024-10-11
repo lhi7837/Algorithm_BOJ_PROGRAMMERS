abe93cb25abce1e1d82b667e2a58b6a2482f9e66
hhhhhhhhhi <lhi7837@gmail.com>
2024-10-11 10:45:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 8868.47 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        char[] arr = number.toCharArray();
+        int len = arr.length - k;
+        int start = 0;
+        for(int i = 0; i < len; i++) {
+            char max = '0';
+            for(int j = start; j <= i + k; j++) {
+                if(arr[j] > max) {
+                    max = arr[j];
+                    start = j + 1;
+                }
+            }
+            sb.append(Character.toString(max));
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
